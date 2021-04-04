82ad3117cfadd687ec1ac9fd9b968705075b9001
williamag929 <williamag929@gmail.com>
2021-04-04 12:34:00 -0500

    interface items

---
+++ src/models/lectura-item/lectura-item-interface.tsx
@@ -0,0 +1,10 @@
+export interface LecturaItem {
+    lecturaid : number;
+    suscriptorid: string;
+    codigo: string;
+    fecha : any;
+    lectura: number;
+    observacion: string;
+    lat: number;
+    lng: number;
+}
---
+++ src/models/suscriptor-item/suscriptor-item-interface.tsx
@@ -0,0 +1,10 @@
+export interface SuscriptorItem {
+    suscriptorid : number;
+    codigo: string;
+    descripcion : string;
+    direccion: string;
+    estado: string;
+    lat: number;
+    lng: number;
+    medidor : string;
+}
